e07196aafec23950fb9f1f51fd50490a0855ae89
dudududu144 <dudududi144@gmail.com>
2026-08-14 01:26:26 +0300

    feat: export roll module

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,7 @@ export * from './domain/harmony.ts';
 export * from './domain/evolution.ts';
 export * from './domain/pack.ts';
 export * from './domain/velocity.ts';
+export * from './domain/roll.ts';
 export * from './protocol/messages.ts';
 export * from './protocol/codec.ts';
 export * from './engine/audio-driver.ts';
