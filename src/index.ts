bcd0d3326b61b904689285b0cc204f392a1eb610
dudududu144 <dudududi144@gmail.com>
2026-08-13 09:56:11 +0300

    feat: export journey module

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export * from './domain/scene.ts';
 export * from './domain/song.ts';
 export * from './domain/euclidean.ts';
 export * from './domain/preset.ts';
+export * from './domain/journey.ts';
 export * from './protocol/messages.ts';
 export * from './protocol/codec.ts';
 export * from './engine/audio-driver.ts';
