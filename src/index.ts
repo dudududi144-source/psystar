d52f3d4df6dd894f8f61d606d7cffdd3731d04b7
dudududu144 <dudududi144@gmail.com>
2026-08-14 02:29:44 +0300

    feat: export wav encoder

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ export * from './engine/recorder.ts';
 export * from './engine/tap-tempo.ts';
 export * from './engine/midi-input-map.ts';
 export * from './engine/humanizer.ts';
+export * from './engine/wav-encoder.ts';
 export * from './integration/foundation-bridge.ts';
 export * from './integration/device-registry.ts';
 export * from './integration/sync-protocol.ts';
