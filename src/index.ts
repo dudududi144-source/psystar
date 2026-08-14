14767871f2b0af2e42f53b55c6fbc09461c5cb9f
dudududu144 <dudududi144@gmail.com>
2026-08-14 19:32:12 +0300

    feat: export spectrum module

--- src/index.ts
+++ src/index.ts
@@ -51,4 +51,5 @@ export * from './integration/foundation-device.ts';
 export * from './integration/p2p-signaling.ts';
 export * from './ui/state.ts';
 export * from './ui/render.ts';
+export * from './ui/spectrum.ts';
 export * from './app.ts';
