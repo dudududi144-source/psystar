3a1c5304fa4e9c05aa70365981380178f1319a87
dudududu144 <dudududi144@gmail.com>
2026-08-14 04:00:22 +0300

    feat: export transcribe module

--- src/index.ts
+++ src/index.ts
@@ -34,6 +34,7 @@ export * from './engine/tap-tempo.ts';
 export * from './engine/midi-input-map.ts';
 export * from './engine/humanizer.ts';
 export * from './engine/wav-encoder.ts';
+export * from './engine/transcribe.ts';
 export * from './integration/foundation-bridge.ts';
 export * from './integration/device-registry.ts';
 export * from './integration/sync-protocol.ts';
